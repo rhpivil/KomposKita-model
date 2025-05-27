df20ba39db86c933d6af2bc19a2cb9bcae6988ad
rhpivil <delvia.yi39@gmail.com>
2025-05-27 13:36:15 +0700

    fix inference

--- main.py
+++ main.py
@@ -15,26 +15,28 @@ app.add_middleware(
 )
 
 model = tf.saved_model.load("model")
+model_inference = model.signatures["saving_default"]
 
 waste_labels = ["Sampah Organik Basah (LAYAK KOMPOS)", "Sampah Organik Kering (LAYAK KOMPOS)", "Sampah Tidak Layak Kompos"]
 
 
 def preprocess_image(image_bytes):
-    image = tf.io.decode_image(image_bytes, channels=3, expand_animations=False)
+    image = tf.io.decode_image(image_bytes, channels=3)
     image = tf.image.resize(image, [512, 512])
     image = tf.cast(image, tf.float32)
     image = preprocess_input(image)
     image_tensor = tf.expand_dims(image, 0)
-    return image_tensor.numpy().tolist()
+    return image_tensor
 
 
 @app.post("/predict")
-async def predict(file: UploadFile = File(...)):
-    image_bytes = await file.read()
+async def predict(image: UploadFile = File(...)):
+    image_bytes = await image.read()
     image_tensor = preprocess_image(image_bytes)
-    prediction = model(image_tensor)
-    predicted_class = int(np.argmax(prediction[0]))
-    confidence = float(np.max(prediction[0]))
+    prediction = model_inference(image_tensor)
+    prediction_values = list(prediction.values())[0].numpy()[0]
+    predicted_class = int(np.argmax(prediction_values))
+    confidence = float(np.max(prediction_values))
 
     return {
         "predicted_class": predicted_class,
